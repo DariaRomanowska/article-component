134f738516f75227dec08dc0ae22ddfd01d562bd
DariaRomanowska <mmmao2212@gmail.com>
2024-11-18 15:14:34 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,5 @@
+document.getElementById('share').addEventListener('click', () => {
+    document.querySelector('.share-active').classList.toggle('d-none')
+document.querySelector('.share').classList.toggle('share-btn-active')
+})
+
